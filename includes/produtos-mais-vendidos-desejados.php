f8965a657ee2bc98d5d36bf316d3950b276f4258
jonathanbugs <jonathanbugs@outlook.com>
2013-02-27 23:46:34 -0300

    foi

--- includes/produtos-mais-vendidos-desejados.php
+++ includes/produtos-mais-vendidos-desejados.php
@@ -11,5 +11,17 @@
                             </span>
                         </a>
                     </li>
+
+                     <li class="boxProduto">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-2.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Bolsa média - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+                        </a>
+                    </li>
                 </ul>
             </div>
