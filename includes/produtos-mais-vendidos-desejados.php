e37208eac81310ae842bdc5956e0bf70fdbfac1c
jonathanbugs <jonathanbugs@outlook.com>
2013-02-28 00:21:32 -0300

    bora

--- includes/produtos-mais-vendidos-desejados.php
+++ includes/produtos-mais-vendidos-desejados.php
@@ -9,17 +9,176 @@
                                 <span class="divisorProduto">/</span>
                                 <span class="precoAtual">Por R$ 826,90</span>
                             </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">2232</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
                         </a>
                     </li>
 
-                     <li class="boxProduto">
+                    <li class="boxProduto">
                         <a class="linkProdutoLista" href="javascript:">
                             <img class="imagemProduto" src="img/conteudo/produto-2.jpg" alt="" />
                             <span class="boxInformacoesProduto">
-                                <span class="ttProdutoLista">Bolsa média - 4174V33</span>
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
                                 <span class="precoAntigo">De R$ 1.709,90</span>
                                 <span class="divisorProduto">/</span>
                                 <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">2232</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="boxProduto">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-3.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">222</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="boxProduto boxProdutoUltimo">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-4.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">22</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="boxProduto">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-4.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">2232</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="boxProduto">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-3.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">2232</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="boxProduto">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-2.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">222</span><span class="textoCentavos">,00</span>
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="boxProduto boxProdutoUltimo">
+                        <a class="linkProdutoLista" href="javascript:">
+                            <img class="imagemProduto" src="img/conteudo/produto-1.jpg" alt="" />
+                            <span class="boxInformacoesProduto">
+                                <span class="ttProdutoLista">Sapatilha - 4174V33</span>
+                                <span class="precoAntigo">De R$ 1.709,90</span>
+                                <span class="divisorProduto">/</span>
+                                <span class="precoAtual">Por R$ 826,90</span>
+                            </span>
+
+                            <span class="boxParcelas">
+                                <span class="quantidadeVezes">
+                                    <span class="textoVezes">6X</span>
+                                    <span class="textoJuros">s/ juros</span>
+                                </span>
+
+                                <span class="valorParcela">
+                                    <span class="textoValor">22</span><span class="textoCentavos">,00</span>
+                                </span>
                             </span>
                         </a>
                     </li>
