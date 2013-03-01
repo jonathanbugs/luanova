a6f9072f7ab852c38932950cb52afeb8e8d4aec6
jonathanbugs <jonathanbugs@outlook.com>
2013-03-01 12:17:11 -0300

    foi

---
+++ includes/newsletter.php
@@ -0,0 +1,21 @@
+<div class="boxCadastroNovidades">
+    <div class="container clearfix">
+        <span class="textoFashion">FASHION UPDATES</span>
+        <span class="textoCadastro">Receba novidades do mundo da moda em seu e-mail.</span>
+        <form action="javascript:;" method="post" name="cadastro" id="cadastroForm" lang="pt">
+            <div class="relative">
+                <label class="label" for="nome">Nome</label>
+                <input class="inputCadastro" type="text" id="nome" name="nome" />
+            </div>
+            <div class="relative">
+                <label class="label" for="email">E-mail</label>
+                <input class="inputCadastro" type="text" id="email" name="email" />
+            </div>
+            <div class="relative">
+                <button type="submit" class="botaoCadastro replace">
+                    OK
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
--- includes/rodape.php
+++ includes/rodape.php
@@ -69,25 +69,7 @@
             <span class="textoRodape textoRodapeDireitos">© 2012, Luz da Lua. Todos os direitos reservados</span>
             <a class="textoRodape textoRodapeSite" href="http://www.luzdalua.com.br">www.luzdalua.com.br</a>
         </div>
-
-        <div class="boxCadastroNovidades">
-            <span class="textoFashion">FASHION UPDATES</span>
-            <span class="textoCadastro">Receba novidades do mundo da moda em seu e-mail.</span>
-            <form action="javascript:;" method="post" name="cadastro" id="cadastroForm" lang="pt">
-                <div class="relative">
-                    <label class="label" for="nome">Nome</label>
-                    <input class="inputCadastro" type="text" id="nome" name="nome" />
-                </div>
-                <div class="relative">
-                    <label class="label" for="email">E-mail</label>
-                    <input class="inputCadastro" type="text" id="email" name="email" />
-                </div>
-                <div class="relative">
-                    <button type="submit" class="botaoCadastro replace">
-                        OK
-                    </button>
-                </div>
-            </form>
-        </div>
     </div>
+
+    <? include 'includes/newsletter.php';?>
 </div>
