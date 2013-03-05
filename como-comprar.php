607affc6d08dea7ec2165172da251173c02013d5
Giuliano Varriale <giuliano.oca@gmail.com>
2013-03-05 20:23:26 -0300

    123
    123

---
+++ como-comprar.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <!-- titulo -->
+    <title></title>
+    <meta name="DC.title" content="" />
+    <!-- /titulo -->
+
+    <!-- metas tags -->
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="rating" content="general" />
+    <meta name="mssmarttagspreventparsing" content="true" />
+    <meta name="ICBM" content="xx.xxxxxx,xx.xxxxxx" />
+    <meta name="language" content="pt-br"/>
+    <meta name="copyright" content=""/>
+    <meta name="designer" content=""/>
+    <meta name="revisit-after" content="1 Days"/>
+    <meta name="distribution" content="Global"/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="googlebot" content="noarchive"/>
+    <meta http-equiv="imagetoolbar" content="no"/>
+    <!-- /meta tags -->
+
+    <!-- links -->
+    <link rel="shortcut icon" href="favicon.ico" /> <!-- tamanho do favicon: 32x32 -->
+    <!-- /links -->
+
+    <!-- meta tags da secao -->
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="Subject" content=""/>
+    <meta name="Abstract" content=""/>
+    <meta name="DC.subject" lang="pt" content="" />
+    <meta name="DC.description" lang="pt" content="" />
+    <meta name="DC.identifier" scheme="DCTERMS.URI" content="" /> <!-- Colocar URL da p�gina -->
+    <!-- /meta tags da secao -->
+
+    <!-- Google Analytics -->
+    <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    </script>
+    <!-- /Google Analytics -->
+
+    <!-- CSS e JS da secao -->
+        <link rel="stylesheet" type="text/css" media="screen" href="css/como-comprar.css" />
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/plugins.js"></script>
+        <script type="text/javascript" src="js/funcoes.js"></script>
+        <script type="text/javascript" src="js/como-comprar.js"></script>
+    <!-- /CSS e JS da secao -->
+</head>
+
+<body>
+    <div id="pagina">
+        <? include 'includes/topo.php';?>
+
+        <div class="container clearfix">
+            <div class="navegacaoAuxiliar clearfix">
+                <a class="btVoltar" href="javascript:;" title="Voltar">Voltar</a>
+                <ul id="migalhaPao">
+                    <li><a href="javascript:;" title="Página inicial">Página inicial</a></li>
+                    <li class="last"><a href="javascript:;" title="Como Comprar">Como Comprar</a></li>
+                </ul>
+            </div>
+            <div id="blocoComoComprar" class="clearfix">
+                <div class="cabecalhoInternas">
+                    <h2 class="ttCabecalho">Como Comprar</h2>
+                    <span class="categoria catComoComprar replace"></span>
+                </div>
+                
+                <ul class="subMenuComoComprar">
+                    <li><a class="selecionado" href="javascript:;" title="Como comprar">Como comprar</a> |</li>
+                    <li><a href="javascript:;" title="Perguntas frequentes">Perguntas frequentes</a> |</li>
+                    <li><a href="javascript:;" title="Cuidados com o seu produto">Cuidados com o seu produto</a></li>
+                </ul>
+                
+                <ul class="accordionComoComprar">
+                    <li>
+                        <a class="btAbrirAccordion replace" href="javascript:;">Abrir</a>
+                        <h3>1 - Escolhendo os produtos</h3>
+                        <div class="conteudoAccordion">
+                            <p>Selecione através do menu a categoria de produtos desejada, posteriormente, opte por uma subcategoria, que divide os produtos por linhas similares. Escolha a sua e siga a navegação.</p>
+                        </div>
+                    </li>
+                    
+                    <li>
+                        <a class="btAbrirAccordion replace" href="javascript:;">Abrir</a>
+                        <h3>2 - Formas de Busca</h3>
+                        <div class="conteudoAccordion">
+                            <p>Selecione através do menu a categoria de produtos desejada, posteriormente, opte por uma subcategoria, que divide os produtos por linhas similares. Escolha a sua e siga a navegação.</p>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+        <? include 'includes/rodape.php';?>
+    </div>
+</body>
+</html>
