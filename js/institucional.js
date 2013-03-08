9f2733838c29a53fa90065e65f8f983c2c918fa4
jonathanbugs <jonathanbugs@outlook.com>
2013-03-07 23:18:28 -0300

    dsds

--- js/institucional.js
+++ js/institucional.js
@@ -7,26 +7,24 @@ $(document).ready(function(){
 function zoomInstitucional(){
 	$('.linkImgInstitucional').on('click', function(){
 		modalZoomInstitucional($(this));
-
-		$('#centralizaImagem').stop().animate({
-			marginTop: -180,
-			height: 400
-		}, 500, "easeInOutQuad");
 	});
 }
 
 
 function modalZoomInstitucional(ele){
 	var imagem = ele.children('.imagemInstitucionalAmpliada').html();
+
 	$('body').append('<div id="holderInstitucional"><div id="overlayInstitucional"></div>'+
 						'<div id="centralizaImagem">'+
 						'<div id="contentImagemAmpliada">'+
 						'<a id="fecharZoomInstitucional" class="btFecharZoom replace" href="javascript:;">Fechar</a>'+
 						'<div class="boxImagem"><img class="imgServicoModal" src="img/conteudo/'+imagem+'.jpg"></div>'+
+						'<div class="scrollModal"><div class="boxTextoModal"><div class="tituloWrapper"></div>'+
 						'</div>'+
 						'</div>'+
 						'</div>'+
-					 '</div>');
+						'</div>'+
+					'</div>');
 
 	$('#overlayInstitucional, #centralizaImagem').fadeTo(500,0.85).click(function(){
 		$(this).fadeTo(500,0).siblings().hide();
